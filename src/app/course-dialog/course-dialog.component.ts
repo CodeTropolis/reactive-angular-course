5e91241fe9cf609ef2005ec8e2b0b92a1fefb6f1
Michael Moore <michael@Michaels-MacBook-Pro.local>
2022-08-16 05:14:21 -0500

    MessagesService and display message on course-dialog.component.

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -7,6 +7,7 @@ import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Component({
     selector: 'course-dialog',
@@ -14,19 +15,20 @@ import { LoadingService } from '../loading/loading.service';
     styleUrls: ['./course-dialog.component.css'],
     // Because this component is opened by the Material framework, it exists in a
     // completely different component tree than the home component.
-    // Therefore, to use the loading service, we need to inject it into the component.
-    providers: [LoadingService] // This service will be available to this component and its children.],
-    // We now have two instances of the LoginService running. 
-    // This is a problem because we want to use the same instance across the app.
-    // Solved by adding <loading> to the course-dialog.component.html file.
-    // I don't think this is the best solution, but it is per the Udemy tutorial.
-    // Why not make it @Injectable({providedIn: 'root'}) ?
-    // Still using <loading> in the course-dialog.component.html file.
+    // Therefore, to use the loading service, we need to provide it here. Well, this would allow us to 
+    providers: [LoadingService, MessagesService] // This service will be available to this component and its children.
+    // We now have two instances of the LoginService and MessagesService running. 
+
+    // Accoring to Vasco (answered my question) in Q&A, Lecture 16:
+    // This is because the global loading indicator will overlap the whole screen and 
+    // block any clicks while it's on, and that indicator is quite big and is in the 
+    // center of the screen. For loading a small table, or for loading multiple tables separately, 
+    // you might want several separate loading indicators that show up aligned at the center of each table, and 
+    // that don't block clicks on the screen.
 })
 export class CourseDialogComponent implements AfterViewInit {
 
     form: FormGroup;
-
     course:Course;
 
     constructor(
@@ -34,7 +36,8 @@ export class CourseDialogComponent implements AfterViewInit {
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
         private coursesService: CoursesService,
-        private loadingService: LoadingService
+        private loadingService: LoadingService,
+        private messageService: MessagesService
         ) {
 
         this.course = course;
@@ -55,6 +58,14 @@ export class CourseDialogComponent implements AfterViewInit {
     save() {
       const changes = this.form.value;
       const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+        .pipe(
+            catchError(err => {
+                const message = "Could not save course. Please try again later.";
+                console.log(message, err);
+                this.messageService.showErrors(message);
+                return throwError(err); // Replaces observable at line 53.
+            })
+        );
       this.loadingService.showLoaderUntilCompleted(saveCourse$)
         .subscribe(
             (val) => this.dialogRef.close(val),
